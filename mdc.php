4001675db1ed0cc2dba6c4df7f70adaf7f0292b9
Gálffy Tamás <ezittgtx@gmail.com>
2015-10-07 21:15:54 +0200

    Made mdc a bit smarter, can be used to mass-compile files

--- mdc.php
+++ mdc.php
@@ -1,7 +1,61 @@
 <?php
 	require('parsedown/Parsedown.php');
+	require('common.php'); 
 	
-	$text = file_get_contents("php://stdin");
-	$md = new Parsedown(); 
-	echo $md->text($text); 
+	//=========================================================================================
+	//Validate parameters
+	$args = ParseParams($argv);
+	
+	//Template ( HTML frame ) 
+	if(isset($args['params']['template']))
+		$templateString = file_get_contents($args['params']['template']); 
+	else if(isset($args['params']['t']))
+		$templateString = file_get_contents($args['params']['t']); 
+	else
+		$templateString = "<html>\n<head>\n</head>\n<body>\n{0}\n</body>\n</html>";
+	
+	//Output directory
+	if(isset($args['params']['o']))
+		$outputDir = $args['params']['o'];
+	else if(isset($args['params']['out']))
+		$outputDir = $args['params']['out'];
+	else 
+		$outputDir = '';
+
+	//Gather input files 
+	$inputFiles = Array(); 
+	foreach($args["values"] as $f)
+		$inputFiles = array_merge($inputFiles, glob($f));
+		
+	//=========================================================================================
+	//Do work 
+	
+	//Create output directory if needed 
+	if($outputDir != '')
+	{
+		if(!is_dir($outputDir))
+		{
+			if(!mkdir($outputDir, 0x777, true))
+			{
+				echo "Failed to create output directory: $outputDir"; 
+				return -1;
+			}
+		}
+		
+		if(substr($outputDir, -1) != '/' && substr($outputDir, -1) != "\\")
+			$outputDir .= "/";
+	}
+	
+	$md = new Parsedown();
+	foreach($inputFiles as $fname)
+	{
+		$ofname = $outputDir . basename($fname);
+		$ofname = substr($ofname, 0, strrpos($ofname, '.'));
+		$ofname .= ".html";
+		
+		$text = file_get_contents($fname); 
+		$text = $md->text($text);
+		$text = format($templateString, $text); 
+		file_put_contents($ofname, $text); 
+	}
 ?>
