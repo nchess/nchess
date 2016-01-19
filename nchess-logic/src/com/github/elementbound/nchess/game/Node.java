95958601698af313345df7a9e5528266498a0f07
Gálffy Tamás <ezittgtx@gmail.com>
2016-01-19 01:11:04 +0100

    Some further doodling. The project may get refactored into multiple projects and repos

--- nchess-logic/src/com/github/elementbound/nchess/game/Node.java
+++ nchess-logic/src/com/github/elementbound/nchess/game/Node.java
@@ -1,12 +1,27 @@
 package com.github.elementbound.nchess.game;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Node {
+class Node {
 	private long id; 
 	
 	private double x;
 	private double y; 
 	
-	private List<Node> neighbors;
+	private List<Long> neighbors;
+	
+	public Node(long id, double x, double y) {
+		this.id = id;
+		this.x = x; 
+		this.y = y; 
+		
+		//TODO: Organize neighbors on some criteria 
+		this.neighbors = new ArrayList<>();
+	}
+	
+	public void link(long toId) {
+		if(!neighbors.contains(toId))
+			neighbors.add(toId);
+	}
 }
--- nchess-logic/src/com/github/elementbound/nchess/game/Table.java
+++ nchess-logic/src/com/github/elementbound/nchess/game/Table.java
@@ -1,7 +1,34 @@
 package com.github.elementbound.nchess.game;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Table {
-	private List<Node> nodes; 
+	private Map<Long, Node> nodes = new HashMap<>(); 
+	private List<Piece> pieces = new ArrayList<>(); 
+	private Map<Long, Player> players = new HashMap<>(); 
+	
+	public boolean addNode(long id, Node node) {
+		if(!this.hasNode(id)) {
+			nodes.put(id, node);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean hasNode(long id) {
+		return nodes.containsKey(id);
+	}
+	
+	public boolean linkNode(long fromId, long toId) {
+		if(!this.hasNode(fromId) || !this.hasNode(toId))
+			return false;
+		
+		nodes.get(fromId).link(toId);
+		return true; 
+	}
 }
