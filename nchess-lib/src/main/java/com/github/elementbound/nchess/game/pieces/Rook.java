58dbf15de15466d5cadeae90775603973bf41bda
Gálffy Tamás <ezittgtx@gmail.com>
2018-05-06 01:08:15 +0200

    Rook refactor

--- nchess-lib/src/main/java/com/github/elementbound/nchess/game/pieces/Rook.java
+++ nchess-lib/src/main/java/com/github/elementbound/nchess/game/pieces/Rook.java
@@ -1,17 +1,19 @@
 package com.github.elementbound.nchess.game.pieces;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-import com.github.elementbound.nchess.game.Move;
-import com.github.elementbound.nchess.game.Node;
-import com.github.elementbound.nchess.game.Piece;
-import com.github.elementbound.nchess.game.Table;
+import com.github.elementbound.nchess.game.*;
+import com.github.elementbound.nchess.util.GameStateUtils;
+import com.github.elementbound.nchess.util.TableUtils;
 
 public class Rook extends Piece {
 
-	public Rook(long player, long at) {
-		super(player, at);
+	public Rook(Node at, Player player) {
+		super(at, player);
 	}
 
 	@Override
@@ -19,45 +21,40 @@ public class Rook extends Piece {
 		return "rook";
 	}
 
+	private boolean isValidTargetNode(Node target, GameState state) {
+	    return target != null && target.isVisible() && !GameStateUtils.isAllyAtNode(state, target, this);
+    }
+
 	@Override
-	public List<Move> getMoves(Table table) {
-		Node node = table.getNode(at);
-		
-		List<Move> moves = new ArrayList<>();
-		
-		for(int i = 0; i < node.neighborCount(); i++) {
-			double direction = table.linkDirection(this.at, node.neighbor(i));
-			
-			long from = at;
-			long to = table.nodeTowardsDirection(from, direction);
-			
-			while(to >= 0) {
-				if(!table.getNode(to).visible())
-					break;
-				
-				if(table.isNodeOccupiedByAlly(to, this.player))	
-					break;
-				
-				if(table.isNodeOccupiedByEnemy(to, this.player)) {
-					moves.add(new Move(this.at, to));
-					break;
-				}
-				
-				moves.add(new Move(this.at, to));
-				
-				//from = to;
-				long nextTo = table.nodeTowardsDirection(to, direction);
-				if(nextTo == from)
-					break;
-				
-				from = to;
-				to = nextTo;
-				
-				System.out.printf("Direction: %f; %d => %d\n", direction, from, to);
-			}
-		}
-		
-		return moves; 
+	public Set<Move> getMoves(GameState state) {
+		Table table = state.getTable();
+        Set<Node> targetNodes = new HashSet<>();
+
+		at.getNeighbors().forEach(towards -> {
+		    double direction = TableUtils.linkDirection(at, towards);
+
+		    // Start stepping in direction until we hit something or arrive on an invalid node
+		    for(Node target = table.nodeTowardsDirection(at, direction);
+                    isValidTargetNode(target, state);
+                    target = table.nodeTowardsDirection(target, direction)) {
+
+		        // Gather our steps
+                targetNodes.add(target);
+
+                // If we hit an enemy, stop there
+                if(GameStateUtils.isEnemyAtNode(state, target, this))
+                    break;
+            }
+        });
+
+		return targetNodes.stream()
+                .map(to -> new Move(at, to))
+                .collect(Collectors.toSet());
 	}
+
+    @Override
+    public Piece move(Node to) {
+        return new Rook(to, player);
+    }
 
 }
